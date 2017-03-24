06b017f43e4cb271c51b18187d0ee64160598629
David Lall <david.lall718@gmail.com>
2017-03-24 12:31:50 -0400

    Roll a Ball

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	
+	public GameObject player;
+	
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	// LateUpdate runs after all Updates, this is important so that the camera moves
+	// only if the player has actually moved.
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
