82e3825da98d33745c7fcb13502e448c1eb962d3
David Lall <david.lall718@gmail.com>
2017-03-24 18:15:04 -0400

    Maze

---
+++ Maze/Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class EndTrigger : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+
+    }
+}
